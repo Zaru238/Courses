644a47a460fbae2dbca1ca56d5a75dbf777330a5
Kostiantyn Ziabrev <kziabrev@luxoft.com>
2021-01-08 20:43:40 +0200

    Update Stanford Algorithms problems
    - Fix Gtest depricated warning
    - Fix QuickSort logic bug

--- CourseraStanfordAlgorithms/QuickSort/library/include/library/foo.h
+++ CourseraStanfordAlgorithms/QuickSort/library/include/library/foo.h
@@ -1,12 +1,16 @@
 #pragma once
 
+#include <algorithm>
+#include <array>
 #include <cassert>
-
 #include <iterator>
 #include <optional>
+#include <ostream>
 #include <stack>
 #include <type_traits>
 #include <utility>
+
+#include <iostream>
 
 enum class Pivot {
   kFirst,
@@ -15,18 +19,16 @@ enum class Pivot {
 };
 
 template <typename Iterator>
-struct Work{
-  Work (Iterator begin, Iterator end)
-    : begin(begin)
-    , end(end) {}
+struct Work {
+  Work(Iterator begin, Iterator end) : begin(begin), end(end) {}
 
   Iterator begin;
   Iterator end;
 };
 
 template <typename Iterator>
-using OptionalWorks = std::pair<std::optional<Work<Iterator>>,
-                                std::optional<Work<Iterator>>>;
+using OptionalWorks =
+    std::pair<std::optional<Work<Iterator>>, std::optional<Work<Iterator>>>;
 
 template <typename Iterator, Pivot PivotKind>
 OptionalWorks<Iterator> QuickSortHelper(const Work<Iterator>& work) {
@@ -38,18 +40,25 @@ OptionalWorks<Iterator> QuickSortHelper(const Work<Iterator>& work) {
   } else if constexpr (Pivot::kLast == PivotKind) {
     pivot = std::prev(work.end);
   } else if constexpr (Pivot::kMedian == PivotKind) {
-    const auto pivot_pos = (std::distance(work.begin, work.end) - 1)/2;
-    pivot = std::next(work.begin, pivot_pos);
+    const auto pivot_pos = (std::distance(work.begin, work.end) - 1) / 2;
+    std::array<Iterator, 3> candidates{
+        work.begin, std::next(work.begin, pivot_pos), std::prev(work.end, 1)};
+
+    std::sort(
+        candidates.begin(), candidates.end(),
+        [](const auto& first, const auto& second) { return *first < *second; });
+
+    pivot = candidates.at(1);
   } else {
-    assert(false);  // Unhandled Pivot Kind
+    assert(false);
   }
 
-  std::swap (*work.begin, *pivot);
+  std::swap(*work.begin, *pivot);
   auto end_less = std::next(work.begin);
   auto end_more = std::next(work.begin);
 
-  for (;end_more != work.end; ++end_more) {
-    if(*pivot > *end_more) {
+  for (; end_more != work.end; ++end_more) {
+    if (*work.begin > *end_more) {
       std::swap(*end_less, *end_more);
       ++end_less;
     }
@@ -61,7 +70,7 @@ OptionalWorks<Iterator> QuickSortHelper(const Work<Iterator>& work) {
     left.emplace(work.begin, std::prev(end_less));
   }
   std::optional<Work<Iterator>> right;
-  if (std::distance (end_less, work.end) > 1) {
+  if (std::distance(end_less, work.end) > 1) {
     right.emplace(end_less, work.end);
   }
 
@@ -71,14 +80,14 @@ OptionalWorks<Iterator> QuickSortHelper(const Work<Iterator>& work) {
 template <typename Iterator, Pivot PivotKind = Pivot::kFirst>
 size_t QuickSort(Iterator first, Iterator last) {
   // See implementation in Standford algorithms courses
-  // QuickSort Partitioning around a pivor
-  size_t comparisons {};
+  // QuickSort Partitioning around a pivot
+  size_t comparisons{};
   if (std::distance(first, last) < 2) {
     return comparisons;
   }
   std::stack<Work<Iterator>> works;
 
-  Work<Iterator> first_work {first, last};
+  Work<Iterator> first_work{first, last};
   works.push(first_work);
   while (!works.empty()) {
     const auto work = works.top();
--- CourseraStanfordAlgorithms/QuickSort/testrunner/foo.cpp
+++ CourseraStanfordAlgorithms/QuickSort/testrunner/foo.cpp
@@ -1,30 +1,62 @@
-#include <cstdlib>
+#include <gtest/gtest.h>
+#include <library/foo.h>
 
 #include <algorithm>
+#include <cstdlib>
 #include <iostream>
 #include <vector>
 
-#include <gtest/gtest.h>
+class QuickSortFuzzyTest : public ::testing::TestWithParam<int> {};
 
-#include <library/foo.h>
-
-class QuickSortFuzzyTest
-    : public ::testing::TestWithParam<int> {};
-
-TEST_P(QuickSortFuzzyTest, QuickSortTest) {
+TEST_P(QuickSortFuzzyTest, QuickSortTest_FirstPivot) {
   std::vector<size_t> table;
+  using Iter = decltype(table)::iterator;
 
   constexpr size_t kSize = 50;
   table.reserve(kSize);
-  for(size_t i = 0; i < kSize; ++i) {
+  for (size_t i = 0; i < kSize; ++i) {
     table.push_back(std::rand() % 100);
   }
-  auto second_table (table);
+  auto second_table(table);
 
   std::sort(table.begin(), table.end());
-  QuickSort(second_table.begin(), second_table.end());
+  QuickSort<Iter, Pivot::kFirst>(second_table.begin(), second_table.end());
 
   ASSERT_TRUE(table == second_table);
 }
 
-INSTANTIATE_TEST_CASE_P(Suite, QuickSortFuzzyTest, ::testing::Range(1, 1000));
+TEST_P(QuickSortFuzzyTest, QuickSortTest_kLastPivot) {
+  std::vector<size_t> table;
+  using Iter = decltype(table)::iterator;
+
+  constexpr size_t kSize = 50;
+  table.reserve(kSize);
+  for (size_t i = 0; i < kSize; ++i) {
+    table.push_back(std::rand() % 100);
+  }
+  auto second_table(table);
+
+  std::sort(table.begin(), table.end());
+  QuickSort<Iter, Pivot::kLast>(second_table.begin(), second_table.end());
+
+  ASSERT_TRUE(table == second_table);
+}
+
+TEST_P(QuickSortFuzzyTest, QuickSortTest_kMedian) {
+  std::vector<size_t> table;
+  using Iter = decltype(table)::iterator;
+
+  constexpr size_t kSize = 50;
+  table.reserve(kSize);
+  for (size_t i = 0; i < kSize; ++i) {
+    table.push_back(std::rand() % 100);
+  }
+  auto second_table(table);
+
+  std::sort(table.begin(), table.end());
+  QuickSort<Iter, Pivot::kMedian>(second_table.begin(), second_table.end());
+
+  ASSERT_TRUE(table == second_table);
+}
+
+INSTANTIATE_TEST_SUITE_P(Suite, QuickSortFuzzyTest, ::testing::Range(1, 1000));
